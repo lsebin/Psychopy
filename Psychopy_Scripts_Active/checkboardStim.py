32e4f6e0a3afc33a471ba95e617b2a13a03231c6
Sebin Lee <76483462+lsebin@users.noreply.github.com>
2022-07-21 15:46:02 -0400

    time loop & iteration updated
    merged two while/for loop for duration
    minimized the steps inside the iteration to decrease delay in displaying stimuli

--- Psychopy_Scripts_Active/checkboardStim.py
+++ Psychopy_Scripts_Active/checkboardStim.py
@@ -19,10 +19,10 @@ width  = 1920 #currently following the monitor size of ACER
 height = 1080
 grid = 240 # set the width of each grid(ideal if set it as a  common divisor of both width and height)
 subGridNum = 2 #number of subGrid on one side when flickering
-numBoard = 4 #number of checker board(subGrid)
+numBoard = 2 #number of checker board(subGrid)
 numStim = 5 #number of stimulus
 interFlickerDelay = 0.1 #delay time in seconds when flickering, cannot be smaller than 0.014 sec)
-duration = 0.5 #duration time of each stimulus in seconds on monitor
+duration = 0.4 #duration time of each stimulus in seconds on monitor
 isSpike = True #wheter the TTL will be displayed in vertical spike or not
 startSeed = 1 #seed number to start from
 #(0,0) is the bottom left corner of the monitor
@@ -34,7 +34,6 @@ subGrid = int(grid / subGridNum)
 logArrLen = (numpy.floor(height / grid)) * (numpy.floor(width / grid))
 flickTime = numpy.round(interFlickerDelay * monRefreshRate) # delay time flickTime * 1/Refresh rate(HZ) of monitor
 
-logArray = numpy.zeros((int(logArrLen),), dtype=int)
 noise_matrix = 0 * numpy.ones((height, width))
 
 noiseStim = visual.ImageStim(myWin, image = noise_matrix, size = (width, height))
@@ -53,53 +52,55 @@ fileAddress, fileName = logFunction.logFileNameGeneratorAlt()
 logScript(os.getcwd(),os.path.basename(__file__), fileAddress, fileName)
 
 #Functions
-#color the subgrid in black and white    
-def colorSubgrid(x, y, grid, subGridNum, mat):
+#color the subgrid in black and white                                    
+def colorSubgrid_multiple(arr, grid, subGridNum, mat):
     sg = subGrid
-    if (subGridNum % 2 == 0) :
-        for i in range(0, subGridNum):
-            if ((i-1) % 2 == 0):
-                for j in range(0, subGridNum):
-                    if ((j-1) % 2 == 0):
-                        for a in range(int(x + i *sg), int(x+ (i+1) * sg)):
-                            for b in range(int(y + j *sg), int(y+ (j+1) * sg)):
-                                mat[a,b] = 1
-                    else : 
-                        for a in range(int(x + i *sg), int(x+ (i+1) * sg)):
-                            for b in range(int(y + j *sg), int(y+ (j+1) * sg)):
-                                mat[a,b] = -1
-            else : 
-                for j in range(0, subGridNum):
-                    if ((j-1) % 2 == 0):
-                        for a in range(int(x + i *sg), int(x+ (i+1) * sg)):
-                            for b in range(int(y + j *sg), int(y+ (j+1) * sg)):
-                                mat[a,b] = -1
-                    else :
-                        for a in range(int(x + i *sg), int(x+ (i+1) * sg)):
-                            for b in range(int(y + j *sg), int(y+ (j+1) * sg)):
-                                mat[a,b] = 1
-    else : 
-        for i in range(0, subGridNum):
-            if ((i-1) % 2 == 0):
-                for j in range(0, subGridNum):
-                    if ((j-1) % 2 == 0):
-                        for a in range(int(x + (i-1)*sg), int(x+ i * sg)):
-                            for b in range(int(y + (j-1)*sg), int(y+ j * sg)):
-                                mat[a,b] = -1
-                    else : 
-                        for a in range(int(x + (i-1)*sg), int(x+ i * sg)):
-                            for b in range(int(y + (j-1)*sg), int(y+ j * sg)):
-                                mat[a,b] = 1
-            else : 
-                for j in range(0, subGridNum):
-                    if ((j-1) % 2 == 0):
-                        for a in range(int(x + (i-1)*sg), int(x+ i * sg)):
-                            for b in range(int(y + (j-1)*sg), int(y+ j * sg)):
-                                mat[a,b] = 1
-                    else : 
-                        for a in range(int(x + (i-1)*sg), int(x+ i * sg)):
-                            for b in range(int(y + (j-1)*sg), int(y+ j * sg)):
-                                mat[a,b] = -1
+    for n in range(0, len(arr)):
+        if (subGridNum % 2 == 0) :
+            for i in range(0, subGridNum):
+                if ((i-1) % 2 == 0):
+                    for j in range(0, subGridNum):
+                        if ((j-1) % 2 == 0):
+                            for a in range(int(int(arr[n, 0]) + i *sg), int(int(arr[n, 0])+ (i+1) * sg)):
+                                for b in range(int(int(arr[n, 1]) + j *sg), int(int(arr[n, 1])+ (j+1) * sg)):
+                                    mat[a,b] = 1
+                        else : 
+                            for a in range(int(int(arr[n, 0]) + i *sg), int(int(arr[n, 0])+ (i+1) * sg)):
+                                for b in range(int(int(arr[n, 1]) + j *sg), int(int(arr[n, 1])+ (j+1) * sg)):
+                                    mat[a,b] = -1
+                else : 
+                    for j in range(0, subGridNum):
+                        if ((j-1) % 2 == 0):
+                            for a in range(int(int(arr[n, 0]) + i *sg), int(int(arr[n, 0])+ (i+1) * sg)):
+                                for b in range(int(int(arr[n, 1]) + j *sg), int(int(arr[n, 1])+ (j+1) * sg)):
+                                    mat[a,b] = -1
+                        else :
+                            for a in range(int(int(arr[n, 0]) + i *sg), int(int(arr[n, 0])+ (i+1) * sg)):
+                                for b in range(int(int(arr[n, 1]) + j *sg), int(int(arr[n, 1])+ (j+1) * sg)):
+                                    mat[a,b] = 1
+        else : 
+            for i in range(0, subGridNum):
+                if ((i-1) % 2 == 0):
+                    for j in range(0, subGridNum):
+                        if ((j-1) % 2 == 0):
+                            for a in range(int(int(arr[n, 0]) + (i-1)*sg), int(int(arr[n, 0])+ i * sg)):
+                                for b in range(int(int(arr[n, 1]) + (j-1)*sg), int(int(arr[n, 1])+ j * sg)):
+                                    mat[a,b] = -1
+                        else : 
+                            for a in range(int(int(arr[n, 0]) + (i-1)*sg), int(int(arr[n, 0])+ i * sg)):
+                                for b in range(int(int(arr[n, 1]) + (j-1)*sg), int(int(arr[n, 1])+ j * sg)):
+                                    mat[a,b] = 1
+                else : 
+                    for j in range(0, subGridNum):
+                        if ((j-1) % 2 == 0):
+                            for a in range(int(int(arr[n, 0]) + (i-1)*sg), int(int(arr[n, 0])+ i * sg)):
+                                for b in range(int(int(arr[n, 1]) + (j-1)*sg), int(int(arr[n, 1])+ j * sg)):
+                                    mat[a,b] = 1
+                        else : 
+                            for a in range(int(int(arr[n, 0]) + (i-1)*sg), int(int(arr[n, 0])+ i * sg)):
+                                for b in range(int(int(arr[n, 1]) + (j-1)*sg), int(int(arr[n, 1])+ j * sg)):
+                                    mat[a,b] = -1
+
 
 #Generate all possible configuration
 def getConfigArr(checkerNum, leng) :
@@ -133,38 +134,36 @@ def decode(config_str, width, gridSize, checkerNum) :
             checkerArr[ind, 0] = x 
             checkerArr[ind, 1] = y 
             ind = ind + 1
-    
     return checkerArr
 
 ####run#####
+decodedArr = []
 configArr = getConfigArr(numBoard, int(logArrLen))
 
-for na in range(0, numStim):
+for num in range(0, numStim):
     os.chdir(fileAddress)
-    config = configArr[na, 0]
+    c = configArr[num, 0]
     for index in range(1, int(logArrLen)) :
-        config = str(config) + str(configArr[na, index])
-    locArr = decode(str(config), width, grid, numBoard)
-    
-    for n in range(0, numBoard):
-        # Setting location for one subgrid
-        stiX = locArr[n, 0]    
-        stiY = locArr[n, 1]
-        print('Grid '+ str(n) + ' : ' + str(stiX)+ ' high and ' + str(stiY) +  
-        ' right from the bottom right corner')
-        colorSubgrid(int(stiX), int(stiY), grid, subGridNum, noise_matrix)
-        noise_matrix = noise_matrix.reshape((height, width))
-        
-        noiseStim.setImage(noise_matrix)
-        
-        if isSpike:
-            ser.setRTS(True) #stimulus trigger ON
-            ser.setRTS(False) #stimulus trigger OFF
-        else :
-            ser.setRTS(True) #stimulus trigger ON
+        c = str(c) + str(configArr[num, index])
+    x = [c]
+    decodedArr = numpy.append(decodedArr, x, axis = 0)
 
-    print('subgrid size : ' + str(subGrid) + ' , grid size : ' + str(grid))
+clock = core.Clock();
+
+for na in range(0, numStim):
+    config = str(decodedArr[na])
+    locArr = decode(config, width, grid, numBoard)
     
+    # Setting location for one subgrid
+    colorSubgrid_multiple(locArr, grid, subGridNum, noise_matrix)
+    noiseStim.setImage(noise_matrix)
+    
+    if isSpike:
+        ser.setRTS(True) #stimulus trigger ON
+        ser.setRTS(False) #stimulus trigger OFF
+    else :
+        ser.setRTS(True) #stimulus trigger ON
+
     #Logging
     file = open(fileName, "a")
     file.write(str(config))
@@ -172,25 +171,22 @@ for na in range(0, numStim):
     file.close()
 
     #Flickering
-    clock = core.Clock();
     clock.reset()
 
     while clock.getTime() < duration:
         noiseStim.contrast = 1
-        for i in range(0, int(flickTime)):
+        rn = clock.getTime()
+        while clock.getTime() - rn < interFlickerDelay and clock.getTime() <= duration:
             myWin.flip()
-            if clock.getTime() >= duration:
-                break
-        noiseStim.contrast = -1 #invert the color of subgrids
-        for i in range(0, int(flickTime)):
+        noiseStim.contrast = -1
+        rn = clock.getTime()
+        while clock.getTime() - rn < interFlickerDelay and clock.getTime() <= duration:
             myWin.flip()
-            if clock.getTime() >= duration:
-                break
 
     ser.setRTS(False) #stimulus trigger OFF
     
     noise_matrix = 0 * numpy.ones((height, width))
-    logArray = numpy.zeros((int(logArrLen),), dtype=int)
 
 #print file name and address
+print('subgrid size : ' + str(subGrid) + ' , grid size : ' + str(grid))
 print(fileAddress + fileName)
